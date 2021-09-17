91d6e7c2168ac67419b2b72814bf36f76d286c34
Callum Coughlan <callum.d.w.coughlan@gmail.com>
2021-09-17 11:18:52 -0400

    Add to buddyInfo to AddressBook then remove buddyInfo

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -3,7 +3,10 @@ import java.util.*;
 public class AddressBook {
     private LinkedList<BuddyInfo> addressBook;
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        AddressBook addressBook = new AddressBook();
+        BuddyInfo buddyInfo = new BuddyInfo("Billie Jean", "1958 Michael Jackson Rd",  "219-843-3488");
+        addressBook.addBuddy(buddyInfo);
+        addressBook.removeBuddy(buddyInfo);
     }
 
     public void addBuddy(BuddyInfo buddyInfo) {
